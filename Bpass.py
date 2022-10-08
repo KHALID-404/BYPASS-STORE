2695f4cc675657b0bce6bb55874968c3534e39f2
KHALID SHAIFULLAH <94977283+KHALID-CYBER-404@users.noreply.github.com>
2022-10-08 23:01:35 +0600

    Add files via upload

---
+++ Bpass.py
@@ -0,0 +1,169 @@
+import os
+import sys
+import time
+import requests
+import uuid
+os.system('git pull')
+os.system('pkg install curl')
+
+class jalan:
+    
+    def __init__(self, z):
+        pass
+
+def ud():
+    os.system('clear')
+    jalan(logo)
+    print('    [1] FIRST FOLLOW MY GITHUB ACCOUNT')
+    print('    [2] EXIT')
+    opt = input('\n    CHOOSE OPTION : ')
+    if opt == '1':
+        os.system('xdg-open https://github.com/KHALID-CYBER-404')
+        o()
+        return None
+    None('\n\x1b[1;31mEXIT\x1b[0;97m')
+
+ 
+def o():
+    os.system('clear')
+    print(logo)
+    jalan('\t           \033[1;31mRANDOM NUMBER CRACKING')
+    print('')
+    jalan('\x1b[1;32m    [1]\x1b[1;33m RANDOM BD ACCOUNT CRACKING ')
+    jalan('\x1b[1;32m    [2] \x1b[1;32mCONTACT WITH OWNER')
+    jalan('\x1b[1;32m    [3] \x1b[1;32mSUBSCRIBE MY YOUTUBE CHANNEL')
+    jalan('\x1b[1;32m    [4] \x1b[1;32mJOIN MY FACEBOOK GROUP')
+    jalan('\x1b[1;32m    [0] \x1b[1;31mEXIT')
+    opt = input('\n   \x1b[1;32m CHOOSE OPTION : ')
+    if opt == '1':
+        i()
+    if None == '2':
+        os.system('xdg-open https://www.facebook.com/KHALID.CYBER.404')
+        return None
+    if None == '3':
+        os.system('xdg-open https://youtube.com/c/KhaliDTechBd1?utm_source=EKLEiJECCKjOmKnC5IiRIQ')
+        return None
+    if None == '4':
+        os.system('xdg-open fb://group/1514685669011353?ref=share')
+        return None
+    if None == '0':
+        os.system('exit')
+        return None
+    None('\n\x1b[1;31m  CHOOSE VALID OPTION\x1b[0;97m')
+
+
+import os,sys,time,json,random,re,string,platform,base64,uuid
+os.system("git pull")
+from bs4 import BeautifulSoup as sop
+from bs4 import BeautifulSoup
+import requests as ress
+from datetime import date
+from datetime import datetime
+from time import sleep
+from time import sleep as waktu
+try:
+    import requests
+    from concurrent.futures import ThreadPoolExecutor as ThreadPool
+    import mechanize
+    from requests.exceptions import ConnectionError
+except ModuleNotFoundError:
+    os.system('pip install mechanize requests futures bs4==2 > /dev/null')
+    os.system('pip install bs4')
+    
+class jalan:
+    def __init__(self, z):
+        for e in z + "\n":
+            sys.stdout.write(e)
+            sys.stdout.flush()
+            time.sleep(0.009)
+            
+RED = '\033[1;91m'
+WHITE = '\033[1;97m'
+GREEN = '\033[1;32m' #
+YELLOW = '\033[1;33m'
+BLUE = '\033[1;34m'
+ORANGE = '\033[1;35m'
+P = '\x1b[1;97m' # PUTIH
+M = '\x1b[1;91m' # MERAH
+H = '\x1b[1;92m' # HIJAU
+K = '\x1b[1;93m' # KUNING
+B = '\x1b[1;94m' # BIRU
+U = '\x1b[1;95m' # UNGU
+O = '\x1b[1;96m' # BIRU MUDA
+N = '\x1b[0m'    # WARNA MATI
+A = '\x1b[1;90m' # WARNA ABU ABU
+BN = '\x1b[1;107m' # BELAKANG PUTIH
+BBL = '\x1b[1;106m' # BELAKANG BIRU LANGIT
+BP = '\x1b[1;105m' # BELAKANG PINK
+BB = '\x1b[1;104m' # BELAKANG BIRU
+BK = '\x1b[1;103m' # BELAKANG KUNING
+BH = '\x1b[1;102m' # BELAKANG HIJAU
+BM = '\x1b[1;101m' # BELAJANG MERAH
+BA = '\x1b[1;100m' # BELAKANG ABU ABU
+my_color = [
+ P, M, H, K, B, U, O, N]
+warna = random.choice(my_color)
+now = datetime.now()
+dt_string = now.strftime("%H:%M")
+current = datetime.now()
+ta = current.year
+bu = current.month
+ha = current.day
+today = date.today()
+logo =                                          ("""   
+  [1;92m  ┌─────────────────────────────────────────────────────┐
+  [1;92m  │                                                     │
+  [1;92m  │  [1;91m   ██   ██ ██   ██  █████  ██      ██ ██████   [1;92m    │
+  [1;92m  │  [1;92m   ██  ██  ██   ██ ██   ██ ██      ██ ██   ██  [1;92m    │
+  [1;92m  │  [1;93m   █████   ███████ ███████ ██      ██ ██   ██  [1;92m    │
+  [1;92m  │  [1;94m   ██  ██  ██   ██ ██   ██ ██      ██ ██   ██  [1;92m    │
+  [1;92m  │  [1;95m   ██   ██ ██   ██ ██   ██ ███████ ██ ██████   [1;92m    │
+  [1;92m  │                                                     │
+  [1;92m  └─────────────────────────────────────────────────────┘
+
+   [1;92m ┌─────────────────────────────────────────────────────┐
+   [1;92m │          [1;91m[[1;92m+[1;91m][1;92mFREE BYPASS TOOL FOR EVERYONE[1;91m[[1;92m+[1;91m]   [1;92m     │
+   [1;92m └─────────────────────────────────────────────────────┘
+[1;92m    ┌─────────────────────────────────────────────────────┐
+[1;92m    │[[1;91m✔[1;92m] DEVELOPER  :            KHALID SHAIFULLAH        │
+[1;92m    │[[1;91m✔[1;92m] FACEBOOK   :            KHALID SHAIFULLAH        │
+[1;92m    │[[1;91m✔[1;92m] GITHUB     :            KHALID-CYBER-404         │
+[1;92m    │[[1;91m✔[1;92m] WHATSAPP   :            +8801798396843           │
+[1;92m    │[[1;91m✔[1;92m] TOOLS      :            UID CRACK                │
+ [1;92m   └─────────────────────────────────────────────────────┘""")
+
+def clear():
+    os.system('clear')
+    print(logo)
+
+def i():
+    print(logo)
+    jalan('\t           \033[1;31mRANDOM NUMBER CRACKING')
+    print('')
+    jalan('\x1b[1;32m    [1]\x1b[1;33mFB-KING BYPASS')
+    jalan('\x1b[1;32m    [2] \x1b[1;32mAKING BYPASS')
+    jalan('\x1b[1;32m    [3] \x1b[1;32mJUTT BYPASS')
+    jalan('\x1b[1;32m    [4] \x1b[1;32mSPD FILE BYPASS')
+    jalan('\x1b[1;32m    [0] \x1b[1;31mEXIT')
+    opt = input('\n   \x1b[1;32m CHOOSE OPTION : ')
+    if opt == '1':
+        os.system('python FB-KING.py')
+        ud()
+    if None == '2':
+        os.system('python Krs.py')
+        ud()
+    if None == '3':
+        os.system('python JUTT.py')
+        ud()
+    if None == '4':
+        os.system('python spd.py')
+        ud()
+    if None == '0':
+        os.system('exit')
+        ud()
+    None('\n\x1b[1;31m  CHOOSE VALID OPTION\x1b[0;97m')
+
+ 
+ 
+ud()
+ 
